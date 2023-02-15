c96242acde99fc3c286d92d2e9399e0c000acf64
LamanBaghiyeva <laman.baghiyeva.03@mail.ru>
2023-02-16 00:46:44 +0400

    FINAL

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 AOS.init();
+//Menu
 const link = document.querySelector(".link")
 const faBars = document.querySelector(".fa-bars")
+const section=document.querySelector(".introsection")
 
 faBars.addEventListener("click", function(){
   link.classList.toggle("aktiv")
-
+  section.style.paddingTop="0"
 })
 
 
+//Json
 const kart=document.querySelector(".posts")
 console.log(kart)
 
@@ -24,12 +27,12 @@ console.log(frontendgelecekforma)
 for(let i=0; i<frontendgelecekforma.length;i++){
     kart.innerHTML+=`
     <div class="row">
-    <div class="card" style="width: 30rem;">
+    <div class="card">
   <div class="card-body">
     <h2 class="card-title">${frontendgelecekforma[i].head}</h2>
     <p class="card-text middle">${frontendgelecekforma[i].date} <span class="ms-3 me-3">|</span>${frontendgelecekforma[i].tema}</p>
     <p href="#" >${frontendgelecekforma[i].text}</p>
-  </div>
+</div>
 </div>
 </div>`
 
@@ -45,6 +48,16 @@ finally{
 }
 }
 
+//Darkmode
+const moon=document.querySelector(".fa-moon")
+const body=document.querySelector("body")
+moon.addEventListener("click", myFun)
+function myFun(e){
+  e.preventDefault()
+  document.querySelector(".section2").style.background="#000"
+  document.body.classList.add("dark-mode")
+ 
+}
 
 
 
